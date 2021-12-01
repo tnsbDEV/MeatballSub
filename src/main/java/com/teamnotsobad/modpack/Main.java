3ed8ba2c49e3cc6bae48d99bec2f392690f23f59
John Monsen <john@ltr.dev>
2021-12-01 14:13:47 -0500

    Initial Workspace Commit
    Should have a setup environment for an IntelliJ project.

---
+++ src/main/java/com/teamnotsobad/modpack/Main.java
@@ -0,0 +1,31 @@
+package com.teamnotsobad.modpack;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
+public class Main
+{
+    public static final String MODID = "tnsb-modpack";
+    public static final String NAME = "The Modpack";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
